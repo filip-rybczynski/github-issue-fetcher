e41627d7eb0c6aac7a52d370f0a7be4681cabe30
Filip Rybczyński <filipryba@gmail.com>
2024-06-25 15:58:23 +0200

    Sort issues by creation date (desc)

--- lib/issue-fetcher/cli.ex
+++ lib/issue-fetcher/cli.ex
@@ -46,6 +46,7 @@ defmodule IssueFetcher.CLI do
   def process({user, project, _count}) do
     IssueFetcher.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
   end
 
   def decode_response({:ok, body}), do: body
@@ -53,5 +54,12 @@ defmodule IssueFetcher.CLI do
   def decode_response({:error, error}) do
     IO.puts("Error fetching from Github: #{error["message"]}")
     System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
   end
 end
