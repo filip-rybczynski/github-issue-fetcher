3b1be299ecc48a2dfa75d41fee2b5127efcf14fe
Filip Rybczyński <filipryba@gmail.com>
2024-06-27 14:03:02 +0200

    Give option to generate command-line executable

--- lib/issue-fetcher/cli.ex
+++ lib/issue-fetcher/cli.ex
@@ -5,7 +5,7 @@ defmodule IssueFetcher.CLI do
   @moduledoc """
   Handle the command line parsing and the dispatch to the various functions that end up generating a table of the last _n_ issues in a github project 
   """
-  def run(argv) do
+  def main(argv) do
     argv |> parse_args() |> process()
   end
 
--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule IssueFetcher.MixProject do
   def project do
     [
       app: :issue_fetcher,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
@@ -23,6 +24,12 @@ defmodule IssueFetcher.MixProject do
     [
       {:httpoison, "~> 2.2"},
       {:poison, "~> 6.0"}
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: IssueFetcher.CLI
     ]
   end
 end
