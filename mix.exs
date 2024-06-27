0d3bd8b79b827fd69dae38addddd7420ec20ec6b
Filip Rybczyński <filipryba@gmail.com>
2024-06-27 15:49:47 +0200

    Add doc dependencies

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule IssueFetcher.MixProject do
       escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.16",
+      name: "Issue Fetcher",
+      source_url: "https://github.com/filip-rybczynski/issue-fetcher",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,7 +25,9 @@ defmodule IssueFetcher.MixProject do
   defp deps do
     [
       {:httpoison, "~> 2.2"},
-      {:poison, "~> 6.0"}
+      {:poison, "~> 6.0"},
+      {:ex_doc, "~> 0.34.1"},
+      {:earmark, "~> 1.4"}
     ]
   end
 
