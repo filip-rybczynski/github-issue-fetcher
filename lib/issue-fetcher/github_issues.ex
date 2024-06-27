cab5287450e52fc150c19bbcc733d6d248d6462f
Filip Rybczyński <filipryba@gmail.com>
2024-06-27 14:51:55 +0200

    Add logging

--- lib/issue-fetcher/github_issues.ex
+++ lib/issue-fetcher/github_issues.ex
@@ -1,9 +1,12 @@
 defmodule IssueFetcher.GithubIssues do
+  require Logger
+
   # use a module attribute to fetch the value at compile time
   @github_url Application.compile_env(:issue_fetcher, :github_url)
 
   @user_agent [{"User-agent", "Elixir filipryba@gmail.com"}]
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
     issues_url(user, project) |> HTTPoison.get(@user_agent) |> handle_response
   end
 
@@ -12,6 +15,9 @@ defmodule IssueFetcher.GithubIssues do
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       check_for_error(status_code),
       Poison.Parser.parse!(body)
